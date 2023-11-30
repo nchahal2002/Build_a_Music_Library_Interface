b3e7bcca3ea45254d83fbc2df4571f14ac1fed73
Nihal Chahal <nchahal2002@gmail.com>
2023-11-29 22:09:31 -0600

    Made changes to file

---
+++ music_library/src/App.js
@@ -0,0 +1,57 @@
+import { Fragment, useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
+
+function App() {
+  let [search, setSearch] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState([])
+
+  const API_URL = 'https://itunes.apple.com/search?term=the%20grateful%20dead'
+
+  useEffect(() => {
+    const fetchData = async () => {
+      document.title = `${search} Music`
+      const response = await fetch(API_URL + search)
+      const resData = await response.json()
+      if (resData.results.length > 0) {
+        setData(resData.results)
+      } else {
+        setMessage('Not Found')
+      }
+
+    }
+    fetchData()
+  }, [search])
+
+  const handleSearch = (e, term) => {
+		e.preventDefault()
+		setSearch(term)
+	}
+
+  return (
+    <div>
+      {message}
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <Fragment>
+              <SearchBar handleSearch={handleSearch} />
+              <DataContext.Provider value={data}>
+                <Gallery />
+              </DataContext.Provider>
+            </Fragment>
+          } />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App;
